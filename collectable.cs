ac482b04b77f6aa6bb6008ee1effa281eef10073
twotoole <33073140+twotoole@users.noreply.github.com>
2020-08-17 12:34:49 +0100

    Add files via upload

---
+++ collectable.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collectable : MonoBehaviour
+{
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void  OnTriggerExit(Collider other){
+        if(other.gameObject == player){
+        player.GetComponent<thirdPersonMovementv2>().maxEnergy += 0.3f;
+        player.GetComponent<thirdPersonMovementv2>().energyIncrease *= 2f;
+
+
+        Destroy(this.gameObject);
+        }
+    }
+}
